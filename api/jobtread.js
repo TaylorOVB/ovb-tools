21726f6d92d0db72a15197d5a418bbeab37d5ad0
TaylorOVB <taylor@ogdenvalleybuilders.com>
2026-03-21 10:15:14 -0600

    Change module.exports to export default in jobtread.js

--- api/jobtread.js
+++ api/jobtread.js
@@ -6,7 +6,7 @@
 //   { operation: 'createCustomer', params: { name, phone, email, ...fields, notes } }
 //   { operation: 'getOrgInfo' }  ← run once to verify connection + see field IDs
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'OPTIONS') {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
